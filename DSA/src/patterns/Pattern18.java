7170f5dbf6f30f5f9ad156d97242a9e73e768624
suryaraja3 <124482043+suryaraja3@users.noreply.github.com>
2024-07-16 20:44:28 +0530

    Pattern18

--- DSA/src/patterns/Pattern18.java
+++ DSA/src/patterns/Pattern18.java
@@ -1,4 +1,47 @@
 package patterns;
 
 public class Pattern18 {
+//    **********
+//    ****  ****
+//    ***    ***
+//    **      **
+//    *        *
+//    **      **
+//    ***    ***
+//    ****  ****
+//    **********
+public static void main(String[] args) {
+pattern(5);
 }
+static void pattern(int n) {
+    for (int row = 0; row < 2 * n-1 ; row++) {
+        for (int col = 0; col < 2 * n; col++) {
+            if (row < n) {
+                if(col<n-row||col>=n+row){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            else{
+                if(col<=row-n+1||col>=3*n-row-2){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+        }
+        System.out.println();
+
+
+    }
+
+
+
+
+    }
+
+}
+
